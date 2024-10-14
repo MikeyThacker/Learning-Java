ceb609d29707f4095ae0cd6b1c41a705e71f6aff
mikeythacker <mikeythacker@icloud.com>
2024-10-14 17:48:48 +0100

    Conditional Statements

--- week3/src/ConditionalStatements.java
+++ week3/src/ConditionalStatements.java
@@ -1,2 +1,40 @@
+import java.security.KeyStore;
+
 public class ConditionalStatements {
+    void ifStatement() {
+        /*
+        IF statement know as single-alternative selection
+
+        if (<condition>) {
+            <code to execute if true>
+        }
+        <code to execute after checking condition>
+
+        Comparisons for primitive data types:
+        >, <: Greater than, Smaller than
+        >=, <=: Greater/Smaller than or equal to
+        ==: Equal to
+        !=: Not equal to
+
+        For Classes e.g., strings:
+        <string>.equals(<string>): check equality of two strings
+         */
+    }
+
+    void ifElseStatement() {
+        /*
+        if (<condition>) {
+            <code to execute if true>
+        } else {
+         <code to execute if false>
+        }
+         */
+    }
+
+    void logicalOperators(){
+        /*
+        &&: AND
+        ||: OR
+         */
+    }
 }
