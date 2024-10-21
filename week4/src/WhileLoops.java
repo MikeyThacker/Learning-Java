391aef6fc2cebdbd11b12ebe1cf507f8739bff0b
mikeythacker <mikeythacker@icloud.com>
2024-10-21 17:47:56 +0100

    While and Do-While loops

---
+++ week4/src/WhileLoops.java
@@ -0,0 +1,28 @@
+public class WhileLoops {
+    void loops() {
+        /* Three main types:
+            While,
+            Do While
+            For
+         */
+    }
+
+    void whileLoops() {
+        /*
+        while (<condition>) {
+            <code to execute while condition is true>
+        }
+         */
+    }
+
+    void doWhileLoops() {
+        /*
+        Condition is checked after loop has started
+        This means code is always executed at least once
+
+        do {
+            <code to execute>
+        } while (<condition>)
+         */
+    }
+}
