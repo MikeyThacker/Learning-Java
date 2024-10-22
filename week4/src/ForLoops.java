26b391563a0efc54f2370eb5b8494e47f86810c8
mikeythacker <mikeythacker@icloud.com>
2024-10-22 09:28:29 +0100

    For Loops

---
+++ week4/src/ForLoops.java
@@ -0,0 +1,21 @@
+public class ForLoops {
+    void forLoops() {
+        /*
+        for (<initialization>; <condition>; Alteration;) {
+            <code to execute>
+        }
+        e.g.,
+        for (int i = 0; i < 5; i ++) {}
+
+        Can count more than one value:
+        for (int i, int j; i > 0; i --, j --;) {}
+         */
+    }
+
+    void loopControlStatements(){
+        /*
+        break: exits the loop entirely
+        continue: skips current loop iteration
+         */
+    }
+}
