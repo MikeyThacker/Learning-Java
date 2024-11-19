c5397e03fc14873301983bdf2a1b1a3c3a8f0a73
mikeythacker <mikeythacker@icloud.com>
2024-11-19 09:49:09 +0000

    Exceptions

--- week8/src/Exceptions.java
+++ week8/src/Exceptions.java
@@ -1,7 +1,46 @@
 public class Exceptions {
-    void tryCatch() {
+    static void tryCatch() {
+        // To catch any exception:
+        try {
+            int a = 10;
+            int b = 0;
+            System.out.println(a / b);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // To catch specific exceptions
+        try {
+            int c = 1;
+            int d = 0;
+            System.out.println(c / d);
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException");
+        } catch (Exception e) {
+            System.out.println("Other Exception");
+        }
+
+        // The Finally block executes whether there is an exception or not
+        // Do not always need this but can help to clean up
+
+        // Method Throws
+        throwCatch();
     }
 
-    void throwCatch() {
+    static int throwCatch() throws ArithmeticException {
+        // The throws keyword passes off the responsibility of handling the exception to the place that calls the method
+        int a = 10;
+        int b = 0;
+
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        tryCatch();
     }
 }
